48d584a69ba39f54506682fa7bf06c665a496e94
Jacksonlakehhs <42844689+Jacksonlakehhs@users.noreply.github.com>
2018-10-09 13:06:11 -0400

    Updated from Brython Server: 10/9/2018 1:06:10 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Jackson Lake
+Credit: 
 
 Assignment:
 
@@ -10,3 +10,4 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
