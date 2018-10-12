9cb3e694a7da040bee4a61dda21a656a870a90c0
Jacksonlakehhs <42844689+Jacksonlakehhs@users.noreply.github.com>
2018-10-12 13:06:43 -0400

    Updated from Brython Server: 10/12/2018 1:06:42 PM

--- cryptography.py
+++ cryptography.py
@@ -9,5 +9,20 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
-associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+#convert each one to a number that represents it, operate on that number, then convert back to a letter.
 
+associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+l = list(associations)
+print(l)
+
+char = x in list(associations)
+
+associations.find(char)
+associations[index]
+
+first = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+message = ("Message: ")
+key = input("Key: ")
+
+if first == "q":
+    #print("Goodbye! ")
